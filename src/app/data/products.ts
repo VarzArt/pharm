110099d798066a033b46a8568af89b205a83e9ff
Nikita <earl11147@gmail.com>
2026-09-11 13:50:57 +0300

    Was updated promocodes

--- src/app/data/products.ts
+++ src/app/data/products.ts
@@ -57,7 +57,9 @@ export const promoCodes = [
   { code: 'Somova', discountPercent: 10 },
   { code: 'Lerafit', discountPercent: 10 },
   { code: 'Solofit', discountPercent: 10 },
-  { code: 'Akela', discountPercent: 10 },
+  { code: 'Akella', discountPercent: 10 },
+  { code: 'King', discountPercent: 10 },
+  { code: 'SHSAM', discountPercent: 10 },
 ]
 
 export const products: Product[] = [
